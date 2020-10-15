a890e39ed7dc7e804826382358bfc2e80ff98264
Jackson Alves <jackson.alves.marcolino@gmail.com>
2020-10-15 14:48:20 -0300

    Olá mundo

---
+++ Projeto-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
