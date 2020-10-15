b62e308436601398460d8751fdd01880b062e94d
Jackson Alves <jackson.alves.marcolino@gmail.com>
2020-10-15 14:51:52 -0300

    Bom dia!

--- Projeto-test/Program.cs
+++ Projeto-test/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_test
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
